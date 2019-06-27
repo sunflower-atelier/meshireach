b5f07f5efa157d5385aad05f64607e9611eb9e33
aki <akihiko00asumi@gmail.com>
2019-06-27 20:41:26 +0900

    apiを作っていくための下地を整えた(ホントに動くかは知らん)

--- go.mod
+++ go.mod
@@ -3,6 +3,9 @@ module meshireach
 go 1.12
 
 require (
+	firebase.google.com/go v3.8.1+incompatible
 	github.com/gin-gonic/gin v1.4.0
 	github.com/jinzhu/gorm v1.9.8
+	golang.org/x/net v0.0.0-20190503192946-f4e77d36d62c
+	google.golang.org/api v0.3.1
 )
---
+++ middleware/firebase.go
@@ -0,0 +1,48 @@
+package middleware
+
+import (
+	"fmt"
+	"net/http"
+	"os"
+	"strings"
+
+	"golang.org/x/net/context"
+
+	firebase "firebase.google.com/go"
+
+	"github.com/gin-gonic/gin"
+	"google.golang.org/api/option"
+)
+
+func FirebaseAuth() gin.HandlerFunc {
+	return func(ctx *gin.Context) {
+		// サーバー起動時に一回やればいい気がする
+		opt := option.WithCredentialsFile("./key/otameshi-firebase-adminsdk.json")
+		app, err := firebase.NewApp(context.Background(), nil, opt)
+		if err != nil {
+			fmt.Printf("error initializing app: %v", err)
+			os.Exit(1)
+		}
+
+		auth, err := app.Auth(context.Background())
+		if err != nil {
+			fmt.Printf("error: %v\n", err)
+			os.Exit(1)
+		}
+
+		// ヘッダからAuth情報をもらう
+		idToken := strings.Replace(ctx.GetHeader("Authorization"), "Bearer ", "", 1)
+
+		/*token*/
+		token, err := auth.VerifyIDToken(context.Background(), idToken)
+		if err != nil {
+			ctx.String(http.StatusForbidden, "ごめんなのび太．このページ，会員用なんだ．")
+			ctx.Abort()
+			return
+		}
+
+		fmt.Printf("Verified ID token: %v\n", token)
+
+		ctx.Next()
+	}
+}
