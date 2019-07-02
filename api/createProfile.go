7e705e65167e8b541ed07d8d8d88f91d0c731a6c
shoman <shoma216@gmail.com>
2019-07-02 18:31:36 +0900

    add create/edit profile apis

--- api/createProfile.go
+++ api/createProfile.go
@@ -7,19 +7,12 @@ import (
 	"github.com/jinzhu/gorm"
 )
 
-// EditProfile is called when edit user information
-func EditProfile(db *gorm.DB) gin.HandlerFunc {
+// CreateProfile is called when create user information
+func CreateProfile(db *gorm.DB) gin.HandlerFunc {
 	return func(c *gin.Context) {
 		var user db_model.User
 		c.BindJSON(&user)
-
-		// 更新前の情報を取得
-		// FIXME: UserIDはheaderのtokenから取得する
-		var beforeUser = User{user.UserID}
-		db.First(&beforeUser)
-
-		// 更新
-		db.Model(&beforeUser).Updates(user)
+		db.Create(user)
 
 		c.JSON(200, gin.H{
 			"status":   "success",
