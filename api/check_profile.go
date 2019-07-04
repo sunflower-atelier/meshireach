e0093db20b7cff3fd30c4acb3b871c21a892a893
tsubasa <loco7557@gmail.com>
2019-07-04 21:09:01 +0900

    リファクタリングした。

--- api/check_profile.go
+++ api/check_profile.go
@@ -1,34 +1,36 @@
 package api
 
 import (
-	"meshireach/db_model"
+	"meshireach/db/model"
 	"net/http"
 
 	"github.com/gin-gonic/gin"
 	"github.com/jinzhu/gorm"
 )
 
-// CheckProfile はUserIDに対応するデータがあるか確認するメソッド、結果はJSON形式で返す
+// CheckProfile はDBにプロフィールがあるか確認するAPI
 func CheckProfile(db *gorm.DB) gin.HandlerFunc {
 	return func(c *gin.Context) {
-		var user db_model.User
+		var user model.User
 		uid, _ := c.Get("UserID")
 		db.Where("user_id = ? ", uid).First(&user)
-		if user.UserID == uid {
+		if user.ID == uid {
 			c.JSON(http.StatusOK, gin.H{
-				"status":   "Exist",
-				"UserID":   user.UserID,
-				"SearchID": user.SearchID,
-				"Name":     user.Name,
-				"Message":  user.Message,
+				"status":     "Exist",
+				"ID":         user.ID,
+				"FirebaseID": user.FirebaseID,
+				"SearchID":   user.SearchID,
+				"Name":       user.Name,
+				"Message":    user.Message,
 			})
 		} else {
 			c.JSON(http.StatusOK, gin.H{
-				"status":   "NotExist",
-				"UserID":   user.UserID,
-				"SearchID": user.SearchID,
-				"Name":     user.Name,
-				"Message":  user.Message,
+				"status":     "NotExist",
+				"ID":         user.ID,
+				"FirebaseID": user.FirebaseID,
+				"SearchID":   user.SearchID,
+				"Name":       user.Name,
+				"Message":    user.Message,
 			})
 		}
 	}
