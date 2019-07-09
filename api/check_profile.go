bad04dc5aef08f4dc2168816031968e66202b115
tsubasa <loco7557@gmail.com>
2019-07-09 15:17:55 +0900

    条件式の所をfalseに変更

--- api/check_profile.go
+++ api/check_profile.go
@@ -13,7 +13,7 @@ func CheckProfile(db *gorm.DB) gin.HandlerFunc {
 	return func(c *gin.Context) {
 		var user model.User
 		firebaseid := c.MustGet("FirebaseID")
-		if db.Where("firebase_id = ? ", firebaseid).First(&user).RecordNotFound() == true {
+		if db.Where("firebase_id = ? ", firebaseid).First(&user).RecordNotFound() == false {
 			c.JSON(http.StatusOK, gin.H{
 				"status":     "Exist",
 				"ID":         user.ID,
