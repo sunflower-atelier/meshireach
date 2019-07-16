ef2aa0010194b28966c2a073f2e3a43323b6d69d
aki <akihiko00asumi@gmail.com>
2019-07-16 16:23:58 +0900

    fix type of Events.Owner.

--- db/model/event.go
+++ db/model/event.go
@@ -10,7 +10,7 @@ import (
 // Usersは参加者のリスト(joinテーブル名はparticipants)
 type Event struct {
 	gorm.Model
-	Owner    User      `gorm:"column:event_owner"`     // イベント作成者(joinのためのforeign keyを設定すべきかも)
+	Owner    uint      `gorm:"column:event_owner"`     // イベント作成者(joinのためのforeign keyを設定すべきかも)
 	Title    string    `gorm:"column:event_title"`     // タイトル
 	Deadline time.Time `gorm:"column:event_deadline"`  // 開始時刻
 	Users    []*User   `gorm:"many2many:participants"` // 参加者リスト
