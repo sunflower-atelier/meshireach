980e01b06327f79e571a94bfa5acec2622daa07d
shoman <shoma216@gmail.com>
2019-07-26 14:57:17 +0900

    add meshi list api

--- api/event.go
+++ api/event.go
@@ -4,23 +4,35 @@ import (
 	"log"
 	"meshireach/db/model"
 	"net/http"
+	"time"
 
 	"github.com/gin-gonic/gin"
 	"github.com/jinzhu/gorm"
 )
 
+type result struct {
+	Owner    uint      `gorm:"column:event_owner"`
+	Title    string    `gorm:"column:event_title"`
+	Deadline time.Time `gorm:"column:event_deadline"`
+	EventID  uint      `gorm:"column:id"`
+}
+
 // GetAllFriendEvents 友達の飯募集を全取得
 func GetAllFriendEvents(db *gorm.DB) gin.HandlerFunc {
 	return func(c *gin.Context) {
 		user := model.User{FirebaseID: c.MustGet("FirebaseID").(string)}
-		var events []model.Event
-		db.Table("friendships").Where("user_id = ?", user.FirebaseID).Joins("right join events on events.event_owner = friendships.friend_id").Scan(&events)
 
-		log.Printf("events[0].Title=%v", events[0].Title)
+		// UserIDをセット
+		db.First(&user)
+
+		var results []result
+		db.Table("friendships").Where("user_id = ?", user.ID).Select("events.event_owner, events.event_title, events.event_deadline, events.id").Joins("right join events on events.event_owner = friendships.friend_id").Scan(&results)
+
+		log.Printf("results=%v", results)
 
 		c.JSON(http.StatusOK, gin.H{
 			"status": "success",
-			"events": events,
+			"events": results,
 		})
 	}
 }
